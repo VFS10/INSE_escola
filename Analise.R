a355e4b21e4cbcb8c01c5ce740cf58d388bcc31f
Vinicius Ferreira <vinicius10f@gmail.com>
2022-12-14 12:04:50 -0300

    Transformando Base

--- Analise.R
+++ Analise.R
@@ -3,13 +3,25 @@
 install.packages("dplyr")
 install.packages("forcast")
 install.packages("readr")
-
+install.packages("tidyverse") 
 
 library(readr)
 library (dplyr)
 library(forcats)
+library(tidyverse)
 
 setwd("~/Github/INSE_escola")
 
+Dados_INSE <- read.csv("INSE_2019_ESCOLAS.csv",sep =";",encoding = "utf8")
 
-INSE <- read.csv(INSE_2019_ESCOLAS.csv)
+View(Dados_INSE)
+
+summary(Dados_INSE)
+
+Dados_INSE <- Dados_INSE %>% dplyr::select(2,4,5)
+
+
+head(Dados_INSE)
+
+
+
